8bfd600f06b7579cabdf7becba8295e7ee331951
Canario Alan <alan.union.1998@gmail.com>
2025-05-08 22:59:36 -0300

    modifique la animimasion del las tegnologias, modifique el boton hamburguesa del header

--- src/script/proyectos.js
+++ src/script/proyectos.js
@@ -7,7 +7,7 @@ export const proyectos = [
       "Un gran desafio de diseño y desarrollo, con un diseño moderno y minimalista, con un sistema de administración de propiedades y usuarios.",
     desarrollo:
       "Desarrollo de un sistema de administración de propiedades y usuarios, con un diseño moderno y minimalista. El sistema cuenta con un panel de administración para la gestión de propiedades. Este desarrollo un gran desafio  ya que se debía cumplir con los requerimientos del cliente y mis conocimientos no llegaban a cubrir todas las necesidades del proyecto. Por lo que tuve que investigar y aprender nuevas tecnologías para poder cumplir con los requerimientos del cliente.",
-    tecnologia: ["PHP", "CSS3", "MYSSQL", "JavaScript"],
+    tecnologia: ["PHP", "CSS3", "MYSQL", "JavaScript"],
     slug: "inmobiliaria-jdf",
     // github: "",
     link: "https://jdfinmuebles.com/",
