ff6c32dd9d712f69e8ad6669e9465f9f427fd265
Canario Alan <alan.union.1998@gmail.com>
2025-08-19 19:02:28 -0300

    agregue el netlify para poder usarlo desde la plataforma, en el astro config agrege una configuracion para levantar el servidor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from "@tailwindcss/vite";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // Habilita el modo servidor
-  server: {
-    port: 4321
-  },
+  output: 'server', // necesario para usar endpoints
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   }
