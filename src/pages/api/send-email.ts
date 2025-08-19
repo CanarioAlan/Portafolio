2c4c1c341561f5e8c35e1739254d2577fd11efd9
Canario Alan <alan.union.1998@gmail.com>
2025-08-19 18:22:10 -0300

    agregamos el archivo send-email, editamos astro.config y agregamos la configuracion que falta en contacto elimino archivos q no sirben mas

---
+++ src/pages/api/send-email.ts
@@ -0,0 +1,26 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+export async function POST({ request }: { request: Request }) {
+  try {
+    const body = await request.json();
+    const { nombre, email, msj } = body;
+
+    const data = await resend.emails.send({
+      from: 'Portfolio <onboarding@resend.dev>',
+      to: 'alan.union.1998@gmail.com',
+      subject: `Nuevo mensaje de ${nombre}`,
+      html: `
+        <h3>Nuevo mensaje desde el portfolio </h3>
+        <p><b>Nombre:</b> ${nombre}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Mensaje:</b> ${msj}</p>
+      `,
+    });
+
+    return new Response(JSON.stringify({ success: true, data }), { status: 200 });
+  } catch (error: any) {
+    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+  }
+}
